fa12f0eb7a7a96c93f34c815b990a05c57ee79bd
niceoneallround <niceoneallround@gmail.com>
2016-03-17 10:41:56 -0700

    Add utils so can be used across services

---
+++ README.md
@@ -0,0 +1,3 @@
+Contains utils when using JWT
+  - manage a custom property that contains a JSON-LD graph
+  - manage a custom property that contains the privacy pipe
---
+++ test/testJwtUtils.js
@@ -0,0 +1,79 @@
+/*jslint node: true, vars: true */
+var assert = require('assert'),
+    jwtHelpers = require('../lib/jwtUtils').jwtUtils,
+    should = require('should'),
+    util = require('util');
+
+describe('jwtHelpers Tests', function () {
+  'use strict';
+
+  var cryptoProps = {
+    issuer: 'bob.com',
+    jwt: {
+      type: 'HS256',
+      secret: 'secret'
+    }
+  };
+
+  function createTestGraph() {
+    return {
+      '@id': 'http://bogus.domain.com/bogus1',
+      '@type': 'http:/bogus.domain.com/type#Bogus',
+      'http:bogus.domain.com/prop#name': 'heya' };
+  }
+
+  function checkTestGraph(graph) {
+    var canon = createTestGraph();
+    graph.should.have.property('@id', canon['@id']);
+    graph.should.have.property('@type', canon['@type']);
+    graph.should.have.property('http:bogus.domain.com/prop#name', canon['http:bogus.domain.com/prop#name']);
+  }
+
+  describe('1. JWT Tests using HMAC and shared secret', function () {
+
+    it('1.1 sign a request and decode', function () {
+
+      var request, token, decoded;
+
+      request = createTestGraph();
+      token = jwtHelpers.sign(cryptoProps, request);
+      assert(token, 'no token produced');
+      decoded = jwtHelpers.verify(cryptoProps, token);
+      checkTestGraph(jwtHelpers.getPnGraph(decoded));
+    }); //it 1.1
+
+    it('1.2 sign a request and decode with the verify and get graph', function () {
+
+      var request, token, body;
+
+      request = createTestGraph();
+      token = jwtHelpers.sign(cryptoProps, request);
+      assert(token, 'no token produced');
+      body = jwtHelpers.verifyGetPnGraph(cryptoProps, token);
+      checkTestGraph(body);
+    }); //it 1.2
+
+    it('1.3 sign a request with a privacy pipe and decode with the verify and get graph and get privacy pipe', function () {
+
+      var request, token, body, props, decoded, pp;
+
+      props = {};
+      props.privacyPipe = 'https://a_nice_privacy_pipe';
+      request = createTestGraph();
+
+      token = jwtHelpers.sign(cryptoProps, request, props);
+      assert(token, 'no token produced');
+
+      decoded = jwtHelpers.verify(cryptoProps, token);
+      console.log('JWT tests - decoded:%j', decoded);
+
+      body = jwtHelpers.getPnGraph(decoded);
+      body.should.have.property('@id', request['@id']);
+
+      pp = jwtHelpers.getPrivacyPipe(decoded);
+      assert(pp, util.format('no privacy pipe returned:%j?', decoded));
+    }); //it 1.3
+
+  }); // describe 1
+
+}); // describe
