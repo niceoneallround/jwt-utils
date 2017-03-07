01975b22deb07bf7851cf48b58067655b5282c57
niceoneallround <niceoneallround@gmail.com>
2017-03-06 18:03:35 -0800

    Add sign subject query result

--- test/testSignSubjectQueryResult.js
+++ test/testSignSubjectQueryResult.js
@@ -15,7 +15,7 @@ const rsaPrivateKey = readfile('rsa-private.pem');
 const rsaPublicKeyPEM = readfile('rsa-public.pem');
 const rsaX509certPEM = readfile('rsa.x509crt');
 
-describe('1 Sign Subject Query Tests', function () {
+describe('1 Sign Subject Query Result Tests', function () {
   'use strict';
 
   let hs256Options = {
@@ -32,36 +32,39 @@ describe('1 Sign Subject Query Tests', function () {
     x509CertPEM: rsaX509certPEM,
   };
 
-  let subjectQuery = {
+  let subjectQueryResult = {
     '@id': 'http://bogus.domain.com/bogus1',
     '@type': 'http:/bogus.domain.com/type#Bogus',
     'http://bogus.domain.com/prop#name': 'heya', };
 
+  let subjects = ['a', 'b'];
   let privacyPipeId = 'pipe1';
 
   it('1.1 HS256 - should create a JWT containing a subject query and a privacy pipe claim in the payload', function () {
-    let props = { subject: subjectQuery['@id'] };
+    let props = { subject: subjectQueryResult['@id'] };
 
-    let token = JWTUtils.signSubjectQuery(subjectQuery, privacyPipeId, hs256Options, props);
+    let token = JWTUtils.signSubjectQueryResult(subjectQueryResult, subjects, privacyPipeId, hs256Options, props);
     assert(token, 'no token produced');
     let verified = JWTUtils.newVerify(token, hs256Options);
     verified.should.have.property('iss', 'bob.com');
-    verified.should.have.property('sub', subjectQuery['@id']);
-    verified.should.have.property(JWTClaims.PN_JWT_TYPE_CLAIM, JWTType.subjectQuery);
-    verified.should.have.property(JWTClaims.SUBJECT_QUERY_CLAIM, subjectQuery);
+    verified.should.have.property('sub', subjectQueryResult['@id']);
+    verified.should.have.property(JWTClaims.PN_JWT_TYPE_CLAIM, JWTType.subjectQueryResult);
+    verified.should.have.property(JWTClaims.SUBJECT_QUERY_CLAIM, subjectQueryResult);
+    verified.should.have.property(JWTClaims.SUBJECT_CLAIM, subjects);
     verified.should.have.property(JWTClaims.PRIVACY_PIPE_CLAIM, privacyPipeId);
   }); //it 1.1
 
   it('1.2 RS256 - should should create a JWT containing a subject query claim and pipe claim in the payload', function () {
 
-    let props = { subject: subjectQuery['@id'] };
-    let token = JWTUtils.signSubjectQuery(subjectQuery, privacyPipeId, rs256Options, props);
+    let props = { subject: subjectQueryResult['@id'] };
+    let token = JWTUtils.signSubjectQueryResult(subjectQueryResult, subjects, privacyPipeId, rs256Options, props);
     assert(token, 'no token produced');
     let verified = JWTUtils.newVerify(token, rs256Options);
     verified.should.have.property('iss', 'bob.com');
-    verified.should.have.property('sub', subjectQuery['@id']);
-    verified.should.have.property(JWTClaims.PN_JWT_TYPE_CLAIM, JWTType.subjectQuery);
-    verified.should.have.property(JWTClaims.SUBJECT_QUERY_CLAIM, subjectQuery);
+    verified.should.have.property('sub', subjectQueryResult['@id']);
+    verified.should.have.property(JWTClaims.PN_JWT_TYPE_CLAIM, JWTType.subjectQueryResult);
+    verified.should.have.property(JWTClaims.SUBJECT_QUERY_CLAIM, subjectQueryResult);
+    verified.should.have.property(JWTClaims.SUBJECT_CLAIM, subjects);
     verified.should.have.property(JWTClaims.PRIVACY_PIPE_CLAIM, privacyPipeId);
   }); //it 1.2
 
