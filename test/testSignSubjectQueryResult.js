5d5ea0c5269817a05ce1717fd5a1c46aa13d80b4
niceoneallround <niceoneallround@gmail.com>
2017-03-07 13:34:26 -0800

    Return JWTs in query

--- test/testSignSubjectQueryResult.js
+++ test/testSignSubjectQueryResult.js
@@ -37,34 +37,34 @@ describe('1 Sign Subject Query Result Tests', function () {
     '@type': 'http:/bogus.domain.com/type#Bogus',
     'http://bogus.domain.com/prop#name': 'heya', };
 
-  let subjects = ['a', 'b'];
+  let subjectJWTs = ['fake-jwt'];
   let privacyPipeId = 'pipe1';
 
   it('1.1 HS256 - should create a JWT containing a subject query and a privacy pipe claim in the payload', function () {
     let props = { subject: subjectQueryResult['@id'] };
 
-    let token = JWTUtils.signSubjectQueryResult(subjectQueryResult, subjects, privacyPipeId, hs256Options, props);
+    let token = JWTUtils.signSubjectQueryResult(subjectQueryResult, subjectJWTs, privacyPipeId, hs256Options, props);
     assert(token, 'no token produced');
     let verified = JWTUtils.newVerify(token, hs256Options);
     verified.should.have.property('iss', 'bob.com');
     verified.should.have.property('sub', subjectQueryResult['@id']);
     verified.should.have.property(JWTClaims.PN_JWT_TYPE_CLAIM, JWTType.subjectQueryResult);
     verified.should.have.property(JWTClaims.SUBJECT_QUERY_RESULT_CLAIM, subjectQueryResult);
-    verified.should.have.property(JWTClaims.SUBJECT_CLAIM, subjects);
+    verified.should.have.property(JWTClaims.SUBJECT_JWTS_CLAIM, subjectJWTs);
     verified.should.have.property(JWTClaims.PRIVACY_PIPE_CLAIM, privacyPipeId);
   }); //it 1.1
 
   it('1.2 RS256 - should should create a JWT containing a subject query claim and pipe claim in the payload', function () {
 
     let props = { subject: subjectQueryResult['@id'] };
-    let token = JWTUtils.signSubjectQueryResult(subjectQueryResult, subjects, privacyPipeId, rs256Options, props);
+    let token = JWTUtils.signSubjectQueryResult(subjectQueryResult, subjectJWTs, privacyPipeId, rs256Options, props);
     assert(token, 'no token produced');
     let verified = JWTUtils.newVerify(token, rs256Options);
     verified.should.have.property('iss', 'bob.com');
     verified.should.have.property('sub', subjectQueryResult['@id']);
     verified.should.have.property(JWTClaims.PN_JWT_TYPE_CLAIM, JWTType.subjectQueryResult);
     verified.should.have.property(JWTClaims.SUBJECT_QUERY_RESULT_CLAIM, subjectQueryResult);
-    verified.should.have.property(JWTClaims.SUBJECT_CLAIM, subjects);
+    verified.should.have.property(JWTClaims.SUBJECT_JWTS_CLAIM, subjectJWTs);
     verified.should.have.property(JWTClaims.PRIVACY_PIPE_CLAIM, privacyPipeId);
   }); //it 1.2
 
